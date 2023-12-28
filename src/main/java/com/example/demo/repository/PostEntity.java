81d63d7456930a2061acf5c8c636b4f005d1ba89
fing9 <djajsl54@naver.com>
2023-12-28 16:06:34 +0900

    [TEST] PostService 테스트 코드 작성

--- src/main/java/com/example/demo/repository/PostEntity.java
+++ src/main/java/com/example/demo/repository/PostEntity.java
@@ -8,7 +8,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
